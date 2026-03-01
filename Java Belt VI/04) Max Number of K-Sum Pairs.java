b4db9065efeb9836580c3d08f8c6da1e361bbdc4
Abhinav <abhinavravi.shankar.s.112@kalvium.community>
2026-03-01 09:36:00 +0530

    repeat

--- Java Belt VI/04) Max Number of K-Sum Pairs.java
+++ Java Belt VI/04) Max Number of K-Sum Pairs.java
@@ -1,51 +1,29 @@
 //You are given an integer array nums and an integer k. In one operation, you can pick two numbers from the array whose sum equals k
 // and remove them from the array. Your task is to return the maximum number of operations you can perform on the array.
+public static int maxOperations(int[] nums, int k) {
 
-import java.util.*;
+    Arrays.sort(nums);
 
-public class Solution {
+    int left = 0;
+    int right = nums.length - 1;
+    int count = 0;
 
-    public static int maxOperations(int[] nums, int k) {
-        // Your code here
+    while (left < right) {
 
-        Arrays.sort(nums);
+        int sum = nums[left] + nums[right];
 
-        int left = 0;
-        int right = nums.length - 1;
-        int count = 0;
-
-        while (left < right) {
-
-            int sum = nums[left] + nums[right];
-
-            if (sum == k) {
-                count++;
-                left++;
-                right--;
-            } 
-            else if (sum < k) {
-                left++;
-            } 
-            else {
-                right--;
-            }
+        if (sum == k) {
+            count++;
+            left++;
+            right--;
+        } 
+        else if (sum < k) {
+            left++;
+        } 
+        else {
+            right--;
         }
-
-        return count;
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-
-        int[] nums = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
-        }
-
-        System.out.println(maxOperations(nums, k));
-    }
+    return count;
 }
