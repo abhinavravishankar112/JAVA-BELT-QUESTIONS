00be769ee8362f194296f1eee745778aa9a48798
Abhinav <abhinavravi.shankar.s.112@kalvium.community>
2026-03-23 19:06:13 +0530

    question 17

---
+++ Java Belt VI/17) Max Length.java
@@ -0,0 +1,28 @@
+public static int maxLength(List<String> arr) {
+    return helper(arr, "", 0);
+}
+
+public static int helper(List<String> arr, String current, int index) {
+
+    // check if current string has duplicate chars
+    if (!isUnique(current)) return 0;
+
+    int max = current.length();
+
+    for (int i = index; i < arr.size(); i++) {
+        max = Math.max(max, helper(arr, current + arr.get(i), i + 1));
+    }
+
+    return max;
+}
+
+public static boolean isUnique(String s) {
+    int[] freq = new int[26];
+
+    for (char c : s.toCharArray()) {
+        if (freq[c - 'a'] > 0) return false;
+        freq[c - 'a']++;
+    }
+
+    return true;
+}
